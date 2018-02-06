f22558e8371249ba9c845a41047c6469be9bd72e
Bilal Fazlani <bilal_fazlani@external.mckinsey.com>
2018-02-06 22:39:51 +0530

    removed commented code

--- reverseproxy.net/ReverseProxySettings.cs
+++ reverseproxy.net/ReverseProxySettings.cs
@@ -6,33 +6,9 @@ namespace ReverseProxy.net
     {
         public Dictionary<string, Route> Routes { get; set; }
     }
-
-//    public class Routes
-//    {
-//        public HashSet<Route> Routes { get; set; }
-//    }
-
+    
     public class Route
     {
         public string Url { get; set; }
-//        public string Name { get; set; }
-
-//        public override int GetHashCode()
-//        {
-//            return Name.GetHashCode();
-//        }
-//
-//        public override bool Equals(object obj)
-//        {
-//            switch (obj)
-//            {
-//                case null:
-//                    return false;
-//                case Route routeSetting:
-//                    return routeSetting.Name == Name;
-//                default:
-//                    return false;
-//            }
-//        }
     }
 }
